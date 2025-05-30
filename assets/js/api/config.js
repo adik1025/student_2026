f9c496c45f335b9ee7b784605f4b6b71e0d3fc22
ThePencilSharpener <khariakush06@gmail.com>
2025-05-29 22:51:48 -0700

    login fix??? hopefully

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8887";
 } else {
-        pythonURI =  "https://striver.stu.nighthawkcodingsociety.com";
+        pythonURI =  "https://flocker.nighthawkcodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
